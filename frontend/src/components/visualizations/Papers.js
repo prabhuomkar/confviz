41f442ead8f87bf661536c17f6dc0e44f54ee18e
prabhuomkar <prabhuomkar@pm.me>
2020-06-05 18:41:26 +0530

    Switched from tables to list

--- frontend/src/components/visualizations/Papers.js
+++ frontend/src/components/visualizations/Papers.js
@@ -1,74 +1,27 @@
 import React from "react";
-import {
-  Table,
-  DataTable,
-  TableHead,
-  TableRow,
-  TableHeader,
-  TableBody,
-  TableCell,
-  TableContainer,
-} from "carbon-components-react";
+import { UnorderedList, ListItem } from "carbon-components-react";
 import utils from "./../../utils";
 
 const Papers = (props) => {
   const { conference } = props;
   const { id } = conference;
 
-  const headers = [
-    { key: "title", header: "Title" },
-    { key: "authors", header: "Authors" },
-    { key: "link", header: "Link" },
-  ];
-  const rows = utils.getPapers(id).slice(0, 200);
+  const rows = utils.getPapers(id);
 
   return (
     <div>
-      <DataTable
-        rows={rows}
-        headers={headers}
-        render={({
-          rows,
-          headers,
-          getHeaderProps,
-          getRowProps,
-          getTableProps,
-          getTableContainerProps,
-        }) => (
-          <TableContainer
-            title="Papers"
-            description="From ICML 2020"
-            {...getTableContainerProps()}
-          >
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map((header) => (
-                    <TableHeader {...getHeaderProps({ header })}>
-                      {header.header}
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow {...getRowProps({ row })}>
-                    {row.cells.map((cell) =>
-                      cell.id.includes("link") ? (
-                        <TableCell key={cell.id}>
-                          <a href={cell.value}>{cell.value}</a>
-                        </TableCell>
-                      ) : (
-                        <TableCell key={cell.id}>{cell.value}</TableCell>
-                      )
-                    )}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-      />
+      <h4 className="text-underline">Papers</h4>
+      <br />
+      <UnorderedList>
+        {rows.map((paper) => (
+          <ListItem>
+            <strong>{paper.title}</strong> -{" "}
+            <a href={paper.link}>{paper.link}</a>
+            <br />
+            <em>{paper.authors}</em>
+          </ListItem>
+        ))}
+      </UnorderedList>
     </div>
   );
 };
