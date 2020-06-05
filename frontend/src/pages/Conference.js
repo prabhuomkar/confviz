2732dd929274f72890adfe14a66f55618ff9b045
prabhuomkar <prabhuomkar@pm.me>
2020-06-05 19:40:43 +0530

    Conference page updated

--- frontend/src/pages/Conference.js
+++ frontend/src/pages/Conference.js
@@ -15,37 +15,37 @@ const Conference = () => {
   return (
     <>
       {conference ? (
-        <div className="bx--grid bx--grid--condensed">
-          <div className="bx--row">
-            <div className="bx--col bx--col-lg-4 bx--col-md-8 bx--col-xlg-4 bx--col-sm-4">
-              <br />
-              <br />
-              <h2>{id}</h2>
-              <h4>{name}</h4>
-              <br />
-              <h5>
-                Website:{" "}
-                <a target="_blank" rel="noopener noreferrer" href={website}>
-                  {website}
-                </a>
-              </h5>
-              <br />
-              <br />
-              <AcceptanceRate conference={conference} />
-              <br />
-              <br />
+        <>
+          <div className="bx--grid bx--grid--condensed">
+            <div className="bx--row">
+              <div className="bx--col bx--col-lg-4 bx--col-md-8 bx--col-xlg-4 bx--col-sm-4">
+                <br />
+                <br />
+                <h2>{id}</h2>
+                <h4>{name}</h4>
+                <br />
+                <h5>
+                  Website:{" "}
+                  <a target="_blank" rel="noopener noreferrer" href={website}>
+                    {website}
+                  </a>
+                </h5>
+                <br />
+                <br />
+                <AcceptanceRate conference={conference} />
+                <br />
+                <br />
+              </div>
+              <div className="bx--col bx--col-lg-6 bx--offset-lg-1 bx--col-md-8 bx--col-xlg-6 bx--offset-xlg-1 bx--col-sm-4">
+                <WordCloud keywords={keywords} />
+                <Keywords keywords={keywords} />
+              </div>
             </div>
-            <div className="bx--col bx--col-lg-6 bx--offset-lg-1 bx--col-md-8 bx--col-xlg-6 bx--offset-xlg-1 bx--col-sm-4">
-              <WordCloud keywords={keywords} />
-              <Keywords keywords={keywords} />
-            </div>
-          </div>
-          <div className="bx--row">
-            <div className="bx--col bx--col-lg-12 bx--col-md-8 bx--col-xlg-12 bx--col-sm-4">
+            <div className="bx--row">
               <Papers conference={conference} />
             </div>
           </div>
-        </div>
+        </>
       ) : (
         <Error />
       )}
